3281a7212bdc20d646699ca5578365f8ce444a6b
syedibrahim78 <rajarafic49@gmail.com>
2025-03-22 14:27:04 +0530

    conflict solved

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -5,28 +7,32 @@ import Navbar from 'react-bootstrap/Navbar';
 import { CiLogin } from "react-icons/ci";
 import Button from 'react-bootstrap/Button';
 import img1 from '../../assets/logo.png';
-import { Link } from 'react-router-dom';
-import './Navigation.css'
-
+import { Link, Route, Routes } from 'react-router-dom';
+import Login from '../Login/Login';
+import Dashboard from '../../pages/Dashboard/Dashboard';
+import PrivateRoute from '../../pages/Dashboard/PrivateRoute';
+import './Navigation.css';
 
 function Navigation() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <Navbar collapseOnSelect expand="md" className="navbar"  >
+    <>
+    <Navbar collapseOnSelect expand="md" className="navbar" fixed='top'>
       <Container classname="navbar1">
         <Navbar.Brand as={Link} to="/">
             <img src={img1} style={{width:'35px'}}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id=" responsive-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="navigate ms-auto">
-            <Nav.Link  as={Link} to="/" >Home</Nav.Link>
+            <Nav.Link  as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} href="/menu">Menu</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
             <Nav.Link eventKey={2} as={Link} to="/contact">
               Contact
             </Nav.Link>
-            
           </Nav>
           <Nav className='navigate ms-auto'>
           <Button style={{background:'#39DB4A',borderRadius:"20px",color:'white',border:'none'}} className='btn'>Login <CiLogin /></Button>
@@ -34,6 +40,11 @@ function Navigation() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    {showLogin && <Login onClose={() => setShowLogin(false)} />}
+    <Routes>
+      <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+    </Routes>
+    </>
   );
 }
 
